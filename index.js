e69b3ec29d21b45554a7d0b71d22e72e85c72057
Mariana Ito Pinto <marianaitopinto@gmail.com>
2022-04-20 15:08:23 -0300

    version

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+
+const app = express();
+
+const holidays = [
+    { date: "1/1/2022", name: "Confraternização mundial" },
+    { date: "1/3/2022", name: "Carnaval" },
+    { date: "4/17/2022", name: "Páscoa" },
+    { date: "4/21/2022", name: "Tiradentes" },
+    { date: "5/1/2022", name: "Dia do trabalho" },
+    { date: "6/16/2022", name: "Corpus Christi" },
+    { date: "9/7/2022", name: "Independência do Brasil" },
+    { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+    { date: "11/2/2022", name: "Finados" },
+    { date: "11/15/2022", name: "Proclamação da República" },
+    { date: "12/25/2022", name: "Natal" }
+  ];
+
+app.get('/holidays', (req, res) => {
+    res.send(holidays);
+});
+
+app.get('/is-today-holiday', (req, res) => {
+    const today = new Date();
+    let isHoliday = '';
+    holidays.forEach((holiday) => {  
+        if (holiday.date == today.toLocaleDateString("en")) {
+            isHoliday =  `Sim, hoje é ${holiday.name}`;
+        } else {
+            isHoliday = `Não, hoje não é feriado`;
+        }
+    });
+    res.send(isHoliday);
+});
+
+app.get('/holidays/:idMonth', (req, res) => {
+    const month = req.params.idMonth;
+    let holidaysInMonth = [];
+    holidays.forEach((holiday) => {
+        if (holiday.date.split("/")[0] === month) {
+            holidaysInMonth.push(holiday);
+        }
+    });
+    res.send(holidaysInMonth);
+});
+
+app.listen(4000);
